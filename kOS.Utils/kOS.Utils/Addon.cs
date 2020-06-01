86395aaa43d7e41f7efeaebca901ece7cf1fb7a9
tony48 <antolinares01@gmail.com>
2020-06-01 07:57:37 -0400

    add more methods

--- kOS.Utils/kOS.Utils/Addon.cs
+++ kOS.Utils/kOS.Utils/Addon.cs
@@ -1,4 +1,21 @@
-﻿using kOS.AddOns;
+﻿/*
+    Copyright (c) 2020 tony48, 2015-2016 djungelorm
+    This file is part of kOS.Utils.
+
+    kOS.Utils is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    kOS.Utils is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with kOS.Utils.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using kOS.AddOns;
 using kOS.Safe.Encapsulation;
 using kOS.Safe.Encapsulation.Suffixes;
 using UnityEngine;
@@ -7,8 +24,8 @@ using kOS.Suffixed;
 
 namespace kOS.Utils
 {
-    [kOSAddon("SCREEN")]
-    [Safe.Utilities.KOSNomenclature("SCREENAddon")]
+    [kOSAddon("UTILS")]
+    [Safe.Utilities.KOSNomenclature("UTILSAddon")]
     public class Addon : Suffixed.Addon
     {
         public Addon(SharedObjects shared) : base(shared)
@@ -24,11 +41,11 @@ namespace kOS.Utils
             AddSuffix("TrueAnomalyAtUT", new TwoArgsSuffix<ScalarValue, Orbitable, ScalarValue>(TrueAnomalyAtUT));
             AddSuffix("UTAtTrueAnomaly", new TwoArgsSuffix<ScalarValue, Orbitable, ScalarValue>(UTAtTrueAnomaly));
             AddSuffix("EccentricAnomalyAtUT", new TwoArgsSuffix<ScalarValue, Orbitable, ScalarValue>(EccentricAnomalyAtUT));
-
-            
+            AddSuffix("TrueAnomalyAtAN", new TwoArgsSuffix<ScalarValue, Orbitable, Orbitable>(TrueAnomalyAtAN));
+            AddSuffix("TrueAnomalyAtDN", new TwoArgsSuffix<ScalarValue, Orbitable, Orbitable>(TrueAnomalyAtDN));
+            AddSuffix("RelativeInclination", new TwoArgsSuffix<ScalarValue, Orbitable, Orbitable>(RelativeInclination));
         }
-
-        // ReSharper disable once InconsistentNaming
+        
         private ScalarValue MeanAnomalyAtUT(Orbitable obt, ScalarValue ut)
         {
             var percent = obt.Orbit.getObtAtUT(ut) / shared.Vessel.orbit.period;
@@ -60,6 +77,27 @@ namespace kOS.Utils
             return RadToDeg(obt.Orbit.EccentricAnomalyAtUT(ut));
         }
 
+        private ScalarValue TrueAnomalyAtAN(Orbitable obt, Orbitable tgt)
+        {
+            if (ReferenceEquals(tgt, null))
+                throw new ArgumentNullException(nameof(tgt));
+            return ClampAngle180(FinePrint.Utilities.OrbitUtilities.AngleOfAscendingNode(obt.Orbit, tgt.Orbit));
+        }
+
+        private ScalarValue TrueAnomalyAtDN(Orbitable obt, Orbitable tgt)
+        {
+            if (ReferenceEquals(tgt, null))
+                throw new ArgumentNullException(nameof(tgt));
+            return ClampAngle180(FinePrint.Utilities.OrbitUtilities.AngleOfDescendingNode(obt.Orbit, tgt.Orbit));
+        }
+
+        private ScalarValue RelativeInclination(Orbitable obt, Orbitable tgt)
+        {
+            if (ReferenceEquals(tgt, null))
+                throw new ArgumentNullException();
+            return FinePrint.Utilities.OrbitUtilities.GetRelativeInclination(obt.Orbit, tgt.Orbit);
+        }
+
         public override BooleanValue Available()
         {
             return true;
@@ -73,6 +111,22 @@ namespace kOS.Utils
         public double DegToRad(double angle)
         {
             return angle * Math.PI / 180;
+        }
+
+        public double ClampAngle360(double angle)
+        {
+            angle = angle % 360d;
+            if (angle < 0d)
+                angle += 360d;
+            return angle;
+        }
+
+        public double ClampAngle180(double angle)
+        {
+            angle = ClampAngle360(angle);
+            if (angle > 180)
+                angle -= 360;
+            return angle;
         }
     }
 }
