287fde15f2516b4b380063261773b79f2b844520
tony48 <antolinares01@gmail.com>
2020-10-31 16:46:21 -0400

    add license

--- kOS.Utils/kOS.Utils/Socket.cs
+++ kOS.Utils/kOS.Utils/Socket.cs
@@ -1,4 +1,21 @@
-﻿using kOS.AddOns;
+﻿/*
+    Copyright (c) 2020 tony48
+    This file is part of kOS.Utils.
+
+    kOS.Utils is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    kOS.Utils is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with kOS.Utils.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using kOS.AddOns;
 using kOS.Safe.Encapsulation;
 using kOS.Safe.Encapsulation.Suffixes;
 using kOS.Suffixed;
